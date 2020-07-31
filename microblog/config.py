dd8b925de50aca4fee65701eecb07d256b3948fe
FakeAnarchist <vascodvrodrigues@gmail.com>
2020-07-31 23:22:02 +0100

    v0.13

--- microblog/config.py
+++ microblog/config.py
@@ -12,5 +12,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['vascodvrodrigues@gmail.com']
+    ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'pt']
+
